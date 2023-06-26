7b337a18651443986245dfef3898e46e3e59133a
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-26 15:12:50 -0300

    AP3 Concluida

--- ed4_ap3.dart
+++ ed4_ap3.dart
@@ -1,1 +1,21 @@
-void main() {}
+void main() {
+  final lista = [
+    'Dio',
+    'Ozzy',
+    'Max',
+    'Joseph',
+    'Jack',
+    'Caesar',
+    'Michael',
+  ];
+
+  final listaAtualizada = removeElemento(lista, 'Max');
+
+  print("Agora a lista está assim: ${listaAtualizada.join(", ")}");
+}
+
+List<String> removeElemento(List<String>? lista, String? elemento) {
+  lista?.remove(elemento);
+
+  return lista ?? [];
+}
