e22f7a267720ac89b185103f6c323077699d2a6c
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-26 14:46:41 -0300

    AP2 Concluida

--- ed4_ap2.dart
+++ ed4_ap2.dart
@@ -1,1 +1,31 @@
-void main() {}
+void main() {
+  const listaString = [
+    '10',
+    '2XXL7',
+    'JOJ0',
+    '99',
+    '381',
+    'AD44',
+    '47',
+    '2B',
+    '123',
+    '78'
+  ];
+
+  final listaConvertida = converterLista(listaString);
+
+  print("Lista convertida: ${listaConvertida.join(", ")}");
+}
+
+List<int> converterLista(List<String> listaString) {
+  final listaConvertida = <int>[];
+
+  for (var item in listaString) {
+    int? numero = int.tryParse(item);
+
+    numero ??= 0;
+
+    listaConvertida.add(numero);
+  }
+  return listaConvertida;
+}
