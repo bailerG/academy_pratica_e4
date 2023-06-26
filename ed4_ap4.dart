a86f8684bd34a2b5cb3fe3aad556ab4da5fdb6b4
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-26 15:38:03 -0300

    AP4 Concluida

--- ed4_ap4.dart
+++ ed4_ap4.dart
@@ -1,1 +1,17 @@
-void main() {}
+void main() {
+  final preco = 150.0;
+  final precoComDesconto = 95.0;
+
+  final desconto = descobrirDesconto(preco, precoComDesconto);
+
+  print("""
+    O preço inicial era: $preco
+    Ele foi vendido por: $precoComDesconto
+    O desconto foi de: $desconto
+  """);
+}
+
+int descobrirDesconto(double preco, double precoComDesconto) {
+  final desconto = (precoComDesconto * 100) ~/ preco;
+  return 100 - desconto;
+}
