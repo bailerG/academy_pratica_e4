db71fb622a1cc4e8d14b5d816c431823fc7510d6
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-26 14:22:29 -0300

    AP1 Concluida

--- ed4_ap1.dart
+++ ed4_ap1.dart
@@ -1,1 +1,19 @@
-void main() {}
+void main() {
+  final anos = [2016, 1988, 2000, 2100, 2300, 1993];
+
+  for (var ano in anos) {
+    if (ehAnoBissexto(ano)) {
+      print("$ano é um ano bissexto.");
+    } else {
+      print("$ano não é um ano bissexto.");
+    }
+  }
+}
+
+bool ehAnoBissexto(int ano) {
+  if (ano % 100 == 0) {
+    return (ano % 400) == 0;
+  } else {
+    return (ano % 4) == 0;
+  }
+}
